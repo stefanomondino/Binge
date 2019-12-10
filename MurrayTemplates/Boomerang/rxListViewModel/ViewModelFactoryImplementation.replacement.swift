8b4f64ee7bc799eaabe5edddcd422d6696b7b3ae
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-12-10 10:10:45 +0100

    fix bone

--- MurrayTemplates/Boomerang/rxListViewModel/ViewModelFactoryImplementation.replacement.swift
+++ MurrayTemplates/Boomerang/rxListViewModel/ViewModelFactoryImplementation.replacement.swift
@@ -1,7 +1,6 @@
-func {{ name|firstLowercase }}() -> ViewModel {
+func {{ name|firstLowercase }}() -> {{name|firstUppercase}}ViewModel {
         ShowListViewModel(itemViewModelFactory: container.itemViewModelFactory,
                           useCase: container.model.useCases.{{ name|firstLowercase}},
                           styleFactory: container.styleFactory)
     }
 
-    
